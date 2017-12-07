a7d1e03ca012b80f6b997ed79a87cf48804be9f8
Nicnl <nicnl25@gmail.com>
2017-12-07 17:26:21 +0100

    Fix multiple extra_hosts having url_strip_prefix

--- src/oshde/configuration_generator.py
+++ src/oshde/configuration_generator.py
@@ -3,22 +3,10 @@ import oshde.config as config
 def generate_haproxy_configuration(containers_to_run):
     # Génération de la configuration HAProxy
     haproxy_conf = []
+    haproxy_set_paths = {}
     with open('haproxy.cfg') as file:
         for line in [line.rstrip() for line in file.readlines()]:
-            if line == '# OSHDE-HAPROXY-BACKENDS':
-                for container_to_run in containers_to_run:
-                    if container_to_run['http_port'] is None:
-                        continue
-
-                    haproxy_conf.append('backend %s' % container_to_run['name'])
-                    haproxy_conf.append('   mode http')
-                    haproxy_conf.append('   server www1 %s:%s check port %s' % (
-                        container_to_run['haproxy_domain'],
-                        container_to_run['http_port'],
-                        container_to_run['http_port']
-                    ))
-                    haproxy_conf.append('')
-            elif line == '# OSHDE-HAPROXY-FRONTENDS':
+            if line == '# OSHDE-HAPROXY-FRONTENDS':
                 haproxy_acl = []
                 haproxy_uses = []
                 hosts_deny = []
@@ -27,8 +15,9 @@ def generate_haproxy_configuration(containers_to_run):
                     if container_to_run['http_port'] is None:
                         continue
 
+                    path_beg_acl = None
+
                     acl_is_host = 'is_' + container_to_run['name']
-                    hosts_deny.append('!' + acl_is_host)
 
                     extra_hosts = ''
                     for extra_host in container_to_run['extra_hosts']:
@@ -41,40 +30,38 @@ def generate_haproxy_configuration(containers_to_run):
                         extra_hosts
                     ))
 
+                    set_paths = []
                     if container_to_run['url_strip_prefix'] is not None:
-                        haproxy_acl.append('   acl beg_%s path_beg -i %s if %s' % (
-                            acl_is_host,
+                        path_beg_acl = 'beg_' + acl_is_host
+
+                        haproxy_acl.append('   acl %s path_beg -i %s if %s' % (
+                            path_beg_acl,
                             container_to_run['url_strip_prefix'],
                             acl_is_host
                         ))
-                        haproxy_acl.append('   http-request deny if %s !beg_%s' % (
-                            acl_is_host,
-                            acl_is_host
-                        ))
                         if container_to_run['url_add_prefix'] is not None:
-                            haproxy_acl.append('   http-request set-path %s%%[path,regsub(^%s,)] if %s beg_%s' % (
+                            set_paths.append('http-request set-path %s%%[path,regsub(^%s,)]' % (
                                 container_to_run['url_add_prefix'],
-                                container_to_run['url_strip_prefix'],
-                                acl_is_host,
-                                acl_is_host
+                                container_to_run['url_strip_prefix']
                             ))
                         else:
-                            haproxy_acl.append('   http-request set-path /%%[path,regsub(^%s,)] if %s beg_%s' % (
-                                container_to_run['url_strip_prefix'],
-                                acl_is_host,
-                                acl_is_host
+                            set_paths.append('http-request set-path /%%[path,regsub(^%s,)]' % (
+                                container_to_run['url_strip_prefix']
                             ))
 
                     if container_to_run['url_add_prefix'] is not None:
                         if container_to_run['url_strip_prefix'] is None:
-                            haproxy_acl.append('   http-request set-path %s if %s' % (
-                                container_to_run['url_add_prefix'] + '%[path,regsub(^/,)]',
-                                acl_is_host
+                            set_paths.append('http-request set-path %s' % (
+                                container_to_run['url_add_prefix'] + '%[path,regsub(^/,)]'
                             ))
 
-                    haproxy_uses.append('   use_backend %s if %s' % (
+                    haproxy_set_paths[container_to_run['name']] = set_paths
+                    hosts_deny.append('!%s' % (path_beg_acl if path_beg_acl is not None else acl_is_host))
+
+                    haproxy_uses.append('   use_backend %s if %s%s' % (
                         container_to_run['name'],
-                        acl_is_host
+                        acl_is_host,
+                        '' if path_beg_acl is None else (' ' + path_beg_acl)
                     ))
 
                 haproxy_conf.append('bind 0.0.0.0:%s' % str(config.haproxy_port))
@@ -85,6 +72,25 @@ def generate_haproxy_configuration(containers_to_run):
                 haproxy_conf.append('   http-request deny if %s' % ' '.join(hosts_deny))
                 haproxy_conf.append('')
                 haproxy_conf += haproxy_uses
+
+            elif line == '# OSHDE-HAPROXY-BACKENDS':
+                for container_to_run in containers_to_run:
+                    if container_to_run['http_port'] is None:
+                        continue
+
+                    haproxy_conf.append('backend %s' % container_to_run['name'])
+                    haproxy_conf.append('   mode http')
+
+                    for set_path in haproxy_set_paths[container_to_run['name']]:
+                        haproxy_conf.append('   %s' % set_path)
+
+                    haproxy_conf.append('   server www1 %s:%s check port %s' % (
+                        container_to_run['haproxy_domain'],
+                        container_to_run['http_port'],
+                        container_to_run['http_port']
+                    ))
+                    haproxy_conf.append('')
+
             else:
                 haproxy_conf.append(line)
 
